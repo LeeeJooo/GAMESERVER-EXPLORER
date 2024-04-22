854457e8f6b5eabf81a9045a686d559536a70299
Joohyun <ljhp1004@gmail.com>
2024-04-23 01:36:53 +0900

    [SERVER] feat : get data in Redis by key

--- ingameServer/RealTimeServer/src/main/java/Explorer/realtimeserver/serverManagement/MessageHandler.java
+++ ingameServer/RealTimeServer/src/main/java/Explorer/realtimeserver/serverManagement/MessageHandler.java
@@ -1,7 +1,9 @@
 package Explorer.realtimeserver.serverManagement;
 
+import Explorer.realtimeserver.redisConnection.RedisService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 import reactor.netty.NettyInbound;
@@ -11,14 +13,21 @@ import reactor.netty.NettyOutbound;
 public class MessageHandler {
 
     private static final Logger log = LoggerFactory.getLogger(MessageHandler.class);    // Logger
+    @Autowired
+    private RedisService redisService;
 
     public Mono<Void> handleMessage(NettyInbound inbound, NettyOutbound outbound) {
 
         return inbound.receive()
                 .asString()
                 .flatMap(msg -> {
-                    inbound.withConnection(connection -> log.info("connection : {}", connection));  // 메세지를 보낸 클라이언트의 connection
-                    log.info("Received data : {}", msg);                                            // 수신한 메시지 내용
+                    /*
+                     * redis 데이터 조회
+                     * key(connection) 으로 value(info)를 조회
+                     */
+                    inbound.withConnection(connection -> log.info("{} : {}", connection, redisService.readFromRedis(connection.toString())));  // 메세지를 보낸 클라이언트의 connection
+
+                    log.info("Received data : {}", msg);        // 수신한 메시지 내용
                     return outbound.sendString(Mono.just(msg)); // 클라이언트에 송신
 
                     /* 송신 성공 여부 확인
