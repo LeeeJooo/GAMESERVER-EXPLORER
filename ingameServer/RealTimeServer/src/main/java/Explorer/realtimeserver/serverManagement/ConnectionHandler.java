bbd069a58052532946ffedfac5152e222a9f87b4
Joohyun <ljhp1004@gmail.com>
2024-04-23 01:34:22 +0900

    [SERVER] feat : save connection in Redis

--- ingameServer/RealTimeServer/src/main/java/Explorer/realtimeserver/serverManagement/ConnectionHandler.java
+++ ingameServer/RealTimeServer/src/main/java/Explorer/realtimeserver/serverManagement/ConnectionHandler.java
@@ -1,7 +1,9 @@
 package Explorer.realtimeserver.serverManagement;
 
+import Explorer.realtimeserver.redisConnection.RedisService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import reactor.netty.Connection;
 
@@ -11,11 +13,18 @@ import java.util.function.Consumer;
 public class ConnectionHandler implements Consumer<Connection> {
 
 
+    private String info = "client";
     private static final Logger log = LoggerFactory.getLogger(ConnectionHandler.class);
+
+    @Autowired
+    private RedisService redisService;
 
     @Override
     public void accept(Connection connection) {
         // client 연결 시 Connection 정보 출력
         log.info("New connection established: {}", connection.channel().remoteAddress());
+
+        // redis에 connection 정보 저장
+        redisService.saveToRedis(connection.toString(), info);
     }
 }
