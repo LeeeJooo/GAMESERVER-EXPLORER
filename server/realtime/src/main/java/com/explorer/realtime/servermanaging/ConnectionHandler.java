aa3d183e25d183b34243d1f312a99b009460fb5f
이주현 <ljhp1004@gmail.com>
2024-05-02 17:22:51 +0900

    [SERVER] feat: Add handlerRemoved in ConnectionHandler

---
+++ server/realtime/src/main/java/com/explorer/realtime/servermanaging/ConnectionHandler.java
@@ -0,0 +1,30 @@
+package com.explorer.realtime.servermanaging;
+
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import reactor.netty.Connection;
+
+import java.util.function.Consumer;
+
+@Slf4j
+@Service
+public class ConnectionHandler implements Consumer<Connection> {
+
+    @Override
+    public void accept(Connection connection) {
+
+        connection.addHandler(new ChannelHandlerAdapter() {
+
+            @Override
+            public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+
+                super.handlerRemoved(ctx);
+                log.info("connection lost: {}", connection.channel().remoteAddress());
+            }
+
+        });
+
+    }
+}
