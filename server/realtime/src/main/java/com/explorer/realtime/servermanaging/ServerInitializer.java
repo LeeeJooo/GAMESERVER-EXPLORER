0882f506d66bfaf0649eb053dd24004716dd9749
이주현 <ljhp1004@gmail.com>
2024-05-02 17:25:54 +0900

    [SERVER] feat: manage connections with ConnectionHandler in ServerInitializer

--- server/realtime/src/main/java/com/explorer/realtime/servermanaging/ServerInitializer.java
+++ server/realtime/src/main/java/com/explorer/realtime/servermanaging/ServerInitializer.java
@@ -13,11 +13,13 @@ public class ServerInitializer {
     private static final int PORT = 1370;
 
     private final RequestHandler requestHandler;
+    private final ConnectionHandler connectionHandler;
 
     public Mono<? extends DisposableServer> initializeServer() {
         return TcpServer
                 .create()                               // create TCPServer instance
                 .port(PORT)                             // set port
+                .doOnConnection(connectionHandler)
                 .handle(requestHandler::handleRequest)  // set up a handler :: handling requests for network connection
                 .bind();                                // bind and start server
     }
