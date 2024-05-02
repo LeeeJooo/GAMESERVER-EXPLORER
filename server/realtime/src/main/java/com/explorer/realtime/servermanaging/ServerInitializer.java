ebbb79b56cce37097d44c7ab5c5af36d473f2f40
이주현 <ljhp1004@gmail.com>
2024-05-02 17:24:33 +0900

    [SERVER] refactor: Refactor ServerInitializer to use Lombok's RequiredArgsConstructor

--- server/realtime/src/main/java/com/explorer/realtime/servermanaging/ServerInitializer.java
+++ server/realtime/src/main/java/com/explorer/realtime/servermanaging/ServerInitializer.java
@@ -1,20 +1,18 @@
 package com.explorer.realtime.servermanaging;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 import reactor.netty.DisposableServer;
 import reactor.netty.tcp.TcpServer;
 
 @Component
+@RequiredArgsConstructor
 public class ServerInitializer {
 
     private static final int PORT = 1370;
 
     private final RequestHandler requestHandler;
-
-    public ServerInitializer(RequestHandler requestHandler) {
-        this.requestHandler = requestHandler;
-    }
 
     public Mono<? extends DisposableServer> initializeServer() {
         return TcpServer
