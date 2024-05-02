2a7f57b5fef0f33dc371295c64a13bacf89ac632
이주현 <ljhp1004@gmail.com>
2024-05-02 14:12:34 +0900

    [SERVER] feat: RequestHandler to ChannelHanlder with multithreading

--- server/chat/src/main/java/com/explorer/chat/servermanaging/RequestHandler.java
+++ server/chat/src/main/java/com/explorer/chat/servermanaging/RequestHandler.java
@@ -1,18 +1,24 @@
 package com.explorer.chat.servermanaging;
 
+import com.explorer.chat.eventhandling.channelhandling.ChannelHandler;
+import lombok.RequiredArgsConstructor;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 import reactor.netty.NettyInbound;
 import reactor.netty.NettyOutbound;
 
 @Component
+@RequiredArgsConstructor
 public class RequestHandler {
 
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
+
+    private final ChannelHandler channelHandler;
 
     public Mono<Void> handleRequest(NettyInbound inbound, NettyOutbound outbound) {
 
@@ -26,15 +32,24 @@ public class RequestHandler {
                     try {
 
                         JSONObject json = new JSONObject(msg);
-                        String type = json.getString("type");
 
-                        switch (type) {
-                            case "session":
-                                break;
+                        inbound.withConnection(connection -> {
 
-                            case "chat":
-                                break;
-                        }
+                            String type = json.getString("type");
+
+                            switch (type) {
+                                case "channel":
+                                    log.info("channel");
+                                    Mono.fromRunnable(() -> channelHandler.channelHandler(json, connection))
+                                            .subscribeOn(Schedulers.boundedElastic())
+                                            .subscribe();
+                                    break;
+
+                                case "chat":
+                                    break;
+                            }
+
+                        });
 
                         return outbound.sendString(Mono.just(msg));
 
