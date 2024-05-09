cbe18048e0581a9e33ad1068938bf16575db3979
Joohyun <ljhp1004@gmail.com>
2024-05-09 23:20:54 +0900

    [SERVER] feat: add Synthesize event logic

---
+++ server/realtime/src/main/java/com/explorer/realtime/gamedatahandling/laboratory/event/Synthesize.java
@@ -0,0 +1,26 @@
+package com.explorer.realtime.gamedatahandling.laboratory.event;
+
+import com.explorer.realtime.gamedatahandling.laboratory.dto.UserInfo;
+import lombok.extern.slf4j.Slf4j;
+import org.json.JSONObject;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Component
+public class Synthesize {
+    public Mono<Void> process(JSONObject json) {
+        /*
+         * 1) Parsing, create UserInfo
+         */
+        UserInfo userInfo = UserInfo.of(json);
+        log.info("userInfo : {} in channel {}", userInfo.getUserId(), userInfo.getChannelId());
+
+        /*
+         * 2) redis-ingame labData에서 합성물이 있는지 확인
+         */
+
+        return Mono.empty();
+
+    }
+}
